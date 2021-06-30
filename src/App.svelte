174abe4f0109f0b57ff9a0a71b0bc04e6a10039b
Vadim Saprykin <sprkweb@yandex.ru>
2021-06-30 23:31:36 +0300

    Init project structure

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" context="module">
+</script>
+
+<script lang="ts">
+    // import { ApolloClient, InMemoryCache } from "@apollo/client";
+    // import { setClient } from "svelte-apollo";
+
+    // export let uri: string;
+
+    // const client = new ApolloClient({
+    //     uri,
+    //     cache: new InMemoryCache()
+    // });
+    // setClient(client);
+</script>
+
+<main>
+	<p>Hey</p>
+</main>
