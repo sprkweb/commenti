faac3bd23fe5b3626eaa0521177a0a8621f5298e
Vadim Saprykin <sprkweb@yandex.ru>
2021-07-05 21:32:24 +0300

    get comments only for current page

--- src/App.svelte
+++ src/App.svelte
@@ -8,15 +8,21 @@
     import Comment from './components/Comment.svelte';
     import { GetComments } from './requests.graphql';
 
-    export let uri: string;
+    export let server_uri: string;
+    export let page_id: string;
 
     const client = new ApolloClient({
-        uri,
+        uri: server_uri,
         cache: new InMemoryCache()
     });
     setClient(client);
 
-    const comments = query<{ allComments: CommentInfo[] }>(GetComments);
+    const comments = query<{ comments: CommentInfo[] }>(
+        GetComments, {
+            variables: {
+                page_id
+            }
+        });
 </script>
 
 <main class="commenti-section">
@@ -25,7 +31,7 @@
     {:else if $comments.error}
         {$comments.error.toString()}
     {:else}
-        {#each $comments.data.allComments as comment}
+        {#each $comments.data.comments as comment}
 	        <Comment comment={comment} />
         {/each}
     {/if}
